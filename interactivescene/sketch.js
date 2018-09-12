06e341d7015ef04568558897242f2e4fe8629acd
ethan-church <31911721+ethan-church@users.noreply.github.com>
2018-09-12 14:09:08 -0600

    Made interactive Rectangle

--- interactivescene/sketch.js
+++ interactivescene/sketch.js
@@ -1,14 +1,28 @@
-// Project Title
-// Your Name
-// Date
+// Drawing Ellipses and Rectangles
+// Ethan Church
+// September 12, 2018
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+    createCanvas(400, 400);
+    background(255);
 }
 
 function draw() {
+  if (mouseIsPressed && keyIsPressed && key === "r"){
+ 	rect(mouseX, mouseY, 100, 75)
+  }
+  if (mouseIsPressed && keyIsPressed && key === "e"){
+ 	ellipse(mouseX, mouseY, 100, 75)
+  }
+}
 
+function keyTyped(){
+  if (key === "w"){
+  	background(255);
+  } else if (key === "b"){
+    background(0);
+  	}
 }
