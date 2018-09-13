1d7e7d403a521cd98836bc2bde5f8473965817c0
ethan-church <31911721+ethan-church@users.noreply.github.com>
2018-09-13 13:39:01 -0600

    Created New Project

---
+++ p5/sketch.js
@@ -0,0 +1,14 @@
+// Project Title
+// Ethan Church
+// September 13, 2018
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+
+}
