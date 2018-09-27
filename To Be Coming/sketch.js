c6c617195aa8de4d79f5f2761f31f933f8c05e4e
unknown <4500279@sps-student.sk.ca>
2018-09-27 14:08:41 -0600

    added Ideas to game design

--- To Be Coming/sketch.js
+++ To Be Coming/sketch.js
@@ -2,9 +2,12 @@
 // Ethan Church
 // September 27, 2018
 //
-// Idea of the game is to be a farmer where you have differnt places you can be...
-// The Shop, Farm Plot, The Barn
-//
+// Idea of the game is to be a farmer where you have differnt places (idea based off of the papa's freezeria you can be in the ...The Shop, Farm Plot, The Barn, etc.
+/*
+The arrow keys will opperate the differnt menu screens, kinda like in papa's freezeria
+For now just starts in the farm with simplified controls like cutting crops, etc.
+Later build on menu screen and creating own chatecter, etc.
+*/
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
