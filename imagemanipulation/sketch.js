eefd607297aaf4a937b60f8f8c010ad937236f27
unknown <4500279@sps-student.sk.ca>
2018-10-09 13:25:10 -0600

    added Image Manipulation

---
+++ imagemanipulation/sketch.js
@@ -0,0 +1,18 @@
+// Image Manipulation
+// Ethan Church
+// October 9 2018
+
+let fish;
+
+function preload(){
+  fish = loadImage("assets/fish.jpeg");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  image(fish, 0, 0);
+}
+
+function draw() {
+
+}
