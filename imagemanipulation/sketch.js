1ac9608fd7b2b2c8085618feb4427cb960d4ca19
unknown <4500279@sps-student.sk.ca>
2018-10-10 13:09:29 -0600

    Update imagemanipulation/sketch.js

--- imagemanipulation/sketch.js
+++ imagemanipulation/sketch.js
@@ -16,8 +16,52 @@ function setup() {
   image(grayFish, 0, 0);
 }
 
-function draw() {
+function keyTyped(){
+  if (key === " ") {
+    image(fish, 0, 0);
+  }
+  if (key === "g") {
+    image(grayFish, 0, 0);
+  }
+}
 
+
+function draw() {
+image(makeSpotLight(fish));
+}
+
+function makeSpotLight(sourceImage) {
+  let img = createImage(sourceImage.width, sourceImage.height);
+
+  img.loadPixels();
+  sourceImage.loadPixels();
+
+  for (let x = 0; x < sourceImage.width; x++) {
+    for (let y = 0; y < sourceImage.height; y++) {
+      let p = sourceImage.get(x, y);
+
+      let r,g,b;
+
+      if (dist(x, y, mouseX, mouseY) < 50){
+        let r = red(p);
+        let g = green(p);
+        let b = blue(p);
+
+      } else {
+        let r = red(0);
+        let g = green(0);
+        let b = blue(0);
+
+      }
+
+      let newPixel = color((r+g+b)/3, (r+g+b)/3, (r+g+b)/3);
+
+      img.set(x, y, newPixel);
+    }
+  }
+
+  img.updatePixels();
+  return img;
 }
 
 function makeGrayscale(sourceImage) {
