47b9e21e788779194815e8218973d04529d77e03
unknown <4500279@sps-student.sk.ca>
2018-10-11 13:57:11 -0600

    Ability to move around screens with arrow keys

--- greenmeadowestates/sketch.js
+++ greenmeadowestates/sketch.js
@@ -25,76 +25,65 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   state = 1;
+  console.log(state);
 }
 
 function draw() {
-  homeScreen();
-  // determineState();
-  // stateChange();
-  // image(menu, 0, 0, windowWidth, windowHeight);
-}
-
-//Creates main home page that opens up when game is started
-function homeScreen() {
-  startScreenBackground();
-  startScreenLogo();
-  startButton();
-  // shopButton();
-  // settingsButton();
+  determineState();
+  stateChange();
 }
 
 //Determines exactly which menu screen the player is in.
 function determineState() {
   if (state === 1) {
-    startmenu();
+    homeScreen();
   } else if (state === 2) {
-    shop();
-  } else if (state === 3) {
     plot1();
-  } else if (state === 4) {
+  } else if (state === 3) {
     plot2();
+  } else if (state === 4) {
+    shop();
   } else {
-    console.log("something really weird happend!")
+    //extras
   }
 }
 
-//START MENU
-
-//Creates start screen background
-function startScreenBackground(){
+//Creates main home page that opens up when game is started
+function homeScreen() {
   image(startscreenbackground, 0, 0, windowWidth, windowHeight);
+  image(logo, 0, 0, windowWidth, windowWidth);
+  startButton();
+  //shopButton();
+  //settingsButton();
 }
 
-//Adds logo to main screen
-function startScreenLogo(){
-  image(logo, 0, 0, windowWidth, windowHeight);
-}
+//START MENU BUTTONS
 
-//Creates start button that is clickable and brings you to different pages
+//Creates start button that is clickable and brings you to farm plot 1
 function startButton() {
   fill(34, 161, 183);
   strokeWeight(windowWidth/300);
   stroke(255);
   rectMode(CENTER);
-  rect(windowWidth/2, windowHeight/2, windowWidth/8, windowHeight/13, 45);
+  rect(windowWidth/2, windowHeight/2, windowWidth/6, windowHeight/10, 45);
   fill(0);
   noStroke();
   textAlign(CENTER,CENTER);
-  textSize(windowWidth/35);
-  text("Start", windowWidth/2, windowHeight/2);
+  textSize(windowWidth/27);
+  text("START", windowWidth/2, windowHeight/2);
 }
 
 //Creates clickable button that opens up in app purchases
 function shopButton() {
-  // image(shopbutton, windowWidth/2, windowHeight/1.8, windowWidth/15, windowWidth/15);
-  fill(34, 161, 183);
-  ellipse(windowWidth/2, windowHeight/1.8, windowWidth/15, windowWidth/15)
+  image(shopbutton, windowWidth/2, windowHeight/2, 500, 500);
+  // fill(34, 161, 183);
+  // ellipse(windowWidth/2, windowHeight/1.8, windowWidth/15, windowWidth/15)
 
 }
 
 //Creates clikcable button that opens up differnt settings (game volume settings, idea how to play)
 function settingsButton() {
-
+  image(settingsbutton, windowWidth/2, windowHeight/2, windowWidth/15, windowWidth/15)
 }
 
 //SHOP
@@ -109,7 +98,7 @@ function shop() {
 function plot1() {
   background(255, 255, 0);
   fill(0);
-  textAlign(CENTRE);
+  textAlign(CENTER);
   textSize(80);
   text("to be continued!");
 }
@@ -122,10 +111,15 @@ function plot2() {
   text("to be continued!", windowWidth / 2., windowHeight / 2);
 }
 
+//Changes states with arrow keys
 function stateChange() {
-  if (keyIsPressed && keyCode === LEFT_AAROW) {
-    state === 3;
-  } else if (keyIsPressed && keyCode === RIGHT_ARROW) {
-    state === 4;
+  if (state > 1 && keyIsPressed && keyCode === LEFT_ARROW) {
+    state -= 1;
+    keyIsPressed = false;
+    console.log(state);
+  } else if (state < 4 && keyIsPressed && keyCode === RIGHT_ARROW) {
+    state += 1;
+    keyIsPressed = false;
+    console.log(state);
   }
 }
