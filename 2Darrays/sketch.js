26941b4c5f9eb15cacce4bbc5215e6c38c675e5c
unknown <4500279@sps-student.sk.ca>
2018-10-24 13:57:07 -0600

    kjhgf

--- 2Darrays/sketch.js
+++ 2Darrays/sketch.js
@@ -5,46 +5,58 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let cols = 5;
-let rows = 5;
+let rows = 10;
+let cols = 10;
 let grid;
 let cellSize;
 
 function setup() {
   createCanvas(600, 600);
-  cellSize = windowWidth / cols;
+  cellSize = width / cols;
   grid = createRandom2dArray(cols, rows);
 }
 
 function draw() {
-   background(255);
-   displayGrid();
+  background(255);
+  displayGrid();
 }
 
-function displayGrid(){
-  for (let y = 0; y < rows; y++){
-    for (let x = 0; x < cols; x++){
-      if (grid[y][x] === 0){
+function displayGrid() {
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      if (grid[y][x] === 0) {
         fill(0);
-      } else {
+      }
+      else {
         fill(255);
       }
-      rect(cellSize)
+      rect(x*cellSize, y*cellSize, cellSize, cellSize);
     }
   }
 }
 
-function createRandome2dArray(cols, rows){
+function createRandom2dArray(cols, rows) {
   let randomGrid = [];
-  for (let y = 0; y < rows; y++){
+  for (let y = 0; y < rows; y++) {
     randomGrid.push([]);
-    for (let x = 0; x < cols; x++){
-      if (random(100) < 50){
-        randomGrid[y].push(1);
-      } else {
+    for (let x = 0; x < cols; x++) {
+      if (random(100) < 50) {
+        randomGrid[y].push(0);
+      }
+      else {
         randomGrid[y].push(1);
       }
     }
   }
   return randomGrid;
+}
+
+function mousePressed(){
+  let x = floor(mouseX / cellSize);
+  let y = floor(mouseY / cellSize);
+  if (grid[y][x] === 1){
+    grid[y][x] = 0;
+  } else if (grid[y][x] === 0){
+    grid[y][x] = 1;
+  }
 }
