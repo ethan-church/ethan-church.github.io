3ae10a9e3acb977a909442ca1c7f0a9d3d73c540
unknown <4500279@sps-student.sk.ca>
2018-10-26 13:27:17 -0600

    Stated Farm Plot One Planning

---
+++ greenmedowestatesplot/sketch.js
@@ -0,0 +1,27 @@
+// Green Meadow Estates Farm Plot
+// Ethan Cjhurch
+// October 26 2018
+//
+//Farm plot that as time goes on the fields will grow and you can harvest them.
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let rows = 10;
+let cols = 10;
+let farmGrid = [];
+let cellSize;
+let theWidth, theHeight;
+
+
+function setup() {
+  theWidth = windowWidth;
+  theHeight = (9 / 16) * windowWidth;
+  createCanvas(theWidth, theHeight);
+}
+
+function draw() {
+
+}
+
+function
