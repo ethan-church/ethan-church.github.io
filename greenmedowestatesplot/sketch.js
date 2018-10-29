0bebba9237bd3f9d11d1e37665b346b0be90660a
unknown <4500279@sps-student.sk.ca>
2018-10-29 14:12:23 -0600

    Added Green Color when Clicked

--- greenmedowestatesplot/sketch.js
+++ greenmedowestatesplot/sketch.js
@@ -9,7 +9,7 @@
 
 let rows = 10;
 let cols = 10;
-let farmGrid;
+let grid;
 let cellSize;
 let theWidth, theHeight;
 
@@ -19,20 +19,22 @@ function setup() {
   theHeight = (9 / 16) * windowWidth;
   createCanvas(theWidth, theHeight);
   cellSize = theHeight / cols;
-  farmGrid = createRandom2dArray(cols, rows);
+  grid = createRandom2dArray(cols, rows);
 }
 
 function draw() {
-  // createRandom2dArray();
   displayPlot();
 }
 
+//Creates original array
 function createRandom2dArray(cols, rows){
   let randomGrid = [];
   for (y = 0; y < rows; y++){
     randomGrid.push([]);
     for (let x = 0; x < cols; x++){
-      randomGrid[y].push(1);
+      randomGrid[y].push(
+      whenPlanted: millis(),
+      currentState: 0,);
     }
   }
   return randomGrid;
@@ -42,8 +44,30 @@ function createRandom2dArray(cols, rows){
 function displayPlot(){
   for(let i = 0; i < rows; i++){
     for(let j = 0; j < cols; j++){
-      fill(139, 69, 19);
+      if (grid[i][j] === 0){
+        fill(107,142,35);
+      } else if (grid[i][j] === 1){
+        fill(139, 69, 19);
+      } else if (grid[i][j] === 2){
+        fill(128,128,0);
+      } else if (grid[i][j] === 3){
+        fill(204,195,0);
+      }
       rect(i * cellSize, j * cellSize, cellSize, cellSize);
     }
   }
 }
+
+//changes individual cell change color if you click and plant a seed\
+function mousePressed() {
+  let x = floor(mouseX / cellSize);
+  let y = floor(mouseY / cellSize);
+  if (grid[x][y] === 1) {
+    grid[x][y] = 0;
+  }
+  else if (grid[x][y] === 0) {
+    grid[x][y] = 1;
+  }
+}
+
+//If mouse is dragged change colors
