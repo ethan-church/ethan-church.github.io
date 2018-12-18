82a26b02856530572ad019e2862eb2183e043c4f
Ethan Church <church.ethan.111@gmail.com>
2018-12-18 13:36:10 -0600

    Added Firebase Config

--- majorproject/sketch.js
+++ majorproject/sketch.js
@@ -5,10 +5,25 @@
 let toDoInput;
 
 function setup() {
+  //Initializes Firebase
+  var config = {
+    apiKey: "AIzaSyC1f-4hcTNqkqkSwwGmwbwACqSNHGaXn9I",
+    authDomain: "frame-planning-centre.firebaseapp.com",
+    databaseURL: "https://frame-planning-centre.firebaseio.com",
+    projectId: "frame-planning-centre",
+    storageBucket: "frame-planning-centre.appspot.com",
+    messagingSenderId: "239238211506"
+  };
+  firebase.initializeApp(config);
+
+  //Start of Project
   toDoInput = [];
   createCanvas(windowWidth, windowHeight);
   toDoForm();
 }
+
+let database = firebase.database();
+let ref = database.ref('add')
 
 function draw() {
 
@@ -23,5 +38,12 @@ function toDoForm(){
   userInput.attribute("placeholder", "Add new task");
   submitButton.parent("toDo");
   submitButton.class("btn btn-primary");
+  submitButton.mousePressed();
   toDoInput.push(userInput);
 }
+
+ function submitButton(){
+   let data = {
+      added;
+   }
+ }
