5001bb19e151396555e3749f4806acff78340c2d
unknown <4500279@sps-student.sk.ca>
2018-12-19 14:12:12 -0600

    Local Storage Working. Adding Numbering System

--- majorproject/sketch.js
+++ majorproject/sketch.js
@@ -3,49 +3,31 @@
 // Start December 10, 2018
 
 let itemCounter;
+let userInput;
+let itemNumber
 
 function setup() {
-  //Initializes Firebase
-  // let config = {
-  //   apiKey: "AIzaSyC1f-4hcTNqkqkSwwGmwbwACqSNHGaXn9I",
-  //   authDomain: "frame-planning-centre.firebaseapp.com",
-  //   databaseURL: "https://frame-planning-centre.firebaseio.com",
-  //   projectId: "frame-planning-centre",
-  //   storageBucket: "frame-planning-centre.appspot.com",
-  //   messagingSenderId: "239238211506"
-  // };
-  // firebase.initializeApp(config);
-  // console.log(firebase);
-
-//FireBase Stuff
-// let userInputs = firebase.database();
-// let ref = userInputs.ref('inputs');
-// let data = {
-//   name: "Ethan Church",
-//   task: "Design"
-// }
-// ref.push(data);
-
-//Start of Project
 createCanvas(windowWidth, windowHeight);
 toDoForm();
-}
-
-itemCounter = 0;
-
-function draw(){
+itemCounter = localStorage.getItem(itemNumber);
 }
 
 function toDoForm(){
   let submitButton = createElement("button", "Done");
   let label = createElement("label", "add");
-  let userInput = createElement("input");
+  userInput = createElement("input");
   userInput.parent("toDo");
   userInput.class("form-control mb-3");
   userInput.attribute("placeholder", "Add new task");
   submitButton.parent("toDo");
   submitButton.class("btn btn-primary");
+  submitButton.mousePressed(saveInfo);
+}
 
+function saveInfo() {
   //Saving Information in Local Storage
-  localStorage.setItem(itemCounter, "hello");
+  localStorage.setItem(itemCounter, userInput.value());
+  itemCounter++;
+  localStorage.setItem("itemNumber", itemCounter);
+  console.log("it worked");
 }
