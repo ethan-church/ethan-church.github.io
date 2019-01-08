bc41c0340575b150cc7b439cf49587a582c4d945
unknown <4500279@sps-student.sk.ca>
2019-01-08 14:12:10 -0600

    Potential new way of saving Items in local Storage

--- majorproject/sketch.js
+++ majorproject/sketch.js
@@ -7,9 +7,11 @@ let mytask;
 let task;
 let inputs;
 
+
 function setup(){
   createCanvas(windowWidth, windowHeight);
   formControl();
+  display();
 }
 
 //Creating form and controls
@@ -32,63 +34,28 @@ function store(){
     name: inputs,
   }
   //Saving to local storage
-  if (localStorage.getItem(task.name) === null){
-    taskArray = [];
-    taskArray.push(task.name);
-    localStorage.setItem('task', (task.name));
-  } else {
-    myTasks = (localStorage.getItem('taskArray'));
-    myTasks.push(taskArray);
-    localStorage.setItem('task', myTasks);
-  }
+  localStorage.setItem(3, task);
+  // if (localStorage.getItem(task.name) === null){
+  //   taskArray = [];
+  //   taskArray.push(task.name);
+  //   localStorage.setItem('task', (task.name));
+  // } else {
+  //   myTasks = (localStorage.getItem('taskArray'));
+  //   myTasks.push(taskArray);
+  //   localStorage.setItem('task', myTasks);
+  // }
   display();
 }
 
 function display(){
-  output = localStorage.getItem(task);
-  outputFinal = createElement("label", output);
-  outputFinal.parent("output");
-  console.log(output);
+  for (let i = 0; i < localStorage.length; i++){
+    output = localStorage.getItem(i);
+    outputFinal = createElement("label", output);
+    outputFinal.parent("output");
+    console.log(output);
+  }
 }
 
+function itemNumber(){
 
-
-// First Save
-
-// let itemCounter;
-// let userInput;
-// let userOutput;
-//
-// function setup() {
-// createCanvas(windowWidth, windowHeight);
-// form();
-// itemCounter = localStorage.getItem("itemNumber");
-// tasks();
-// }
-//
-// function form(){
-//   let submitButton = createElement("button", "Done");
-//   userInput = createElement("input");
-//   userInput.parent("toDo");
-//   userInput.class("form-control mb-3");
-//   userInput.attribute("placeholder", "Add new task");
-//   submitButton.parent("toDo");
-//   submitButton.class("btn btn-primary");
-//  submitButton.mousePressed(store);
-// }
-//
-// function store() {
-//   //Saving Information in Local Storage
-//   localStorage.setItem(itemCounter, userInput.value());
-//   localStorage.setItem("itemNumber", itemCounter);
-//   itemCounter++;
-//   getValues();
-// }
-//
-// function getValues(){
-//   for (let i = 0; i < localStorage.length; i++){
-//   userOutput = createElement("label","milk");
-//   userOutput.parent("output");
-//   console.log(localStorage);
-//   }
-// }
+}
