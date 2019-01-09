38266a929c753a3e60dacbc3b74e08f88300181e
unknown <4500279@sps-student.sk.ca>
2019-01-09 14:13:05 -0600

    Major advanments with JSON

--- majorproject/sketch.js
+++ majorproject/sketch.js
@@ -6,12 +6,15 @@ let tasks;
 let mytask;
 let task;
 let inputs;
+let itemNumber, itemInStorage;
 
 
 function setup(){
-  createCanvas(windowWidth, windowHeight);
+  itemNumber = 0;
+  itemInStorage = true;
   formControl();
   display();
+  newItem();
 }
 
 //Creating form and controls
@@ -23,7 +26,7 @@ function formControl(){
     userInput.attribute("placeholder", "Add new task");
     submitButton.parent("toDo");
     submitButton.class("btn btn-primary");
-   submitButton.mousePressed(store);
+    submitButton.keyPressed();
 }
 
 //Storing information
@@ -34,7 +37,7 @@ function store(){
     name: inputs,
   }
   //Saving to local storage
-  localStorage.setItem(3, task);
+  localStorage.setItem(itemNumber(), task);
   // if (localStorage.getItem(task.name) === null){
   //   taskArray = [];
   //   taskArray.push(task.name);
@@ -49,13 +52,66 @@ function store(){
 
 function display(){
   for (let i = 0; i < localStorage.length; i++){
-    output = localStorage.getItem(i);
-    outputFinal = createElement("label", output);
+    output = localStorage.getItem(1);
+    outputFinal = createElement("input", output);
     outputFinal.parent("output");
+    outputFinal.class("form-check-input");
     console.log(output);
   }
 }
 
-function itemNumber(){
-
+function newItem(){
+  while (itemInStorage) {
+      if (localStorage.getItem(itemNumber) === null) {
+          itemInStorage = false;
+      }
+      else {
+          itemNumber += 1;
+      }
+  }
 }
+
+function keyPressed(){
+  if (keyCode === Enter){
+    store();
+  } else if (keyCode === ){
+    store();
+  }
+}
+
+
+// let task = {name: "Buy Milk", due: "tomorrow", who: "me"}
+// undefined
+// task.due
+// "tomorrow"
+// task.name
+// "Buy Milk"
+// JSON.stringify(task)
+// "{"name":"Buy Milk","due":"tomorrow","who":"me"}"
+// let testingThing = JSON.stringify(task)
+// undefined
+// JSON.parse(testingThing).due
+// "tomorrow"
+// localStorage.setItem(6, testingThing);
+// undefined
+// localStorage.getItem(6)
+// "{"name":"Buy Milk","due":"tomorrow","who":"me"}"
+// JSON.parse(localStorage.getItem(6)).due
+// "tomorrow"
+// JSON.parse(localStorage.getItem(6)).name
+// "Buy Milk"
+// localStorage.has(0)
+
+
+
+// token = 0;
+// tokenInStorage = true;
+// true
+// while (tokenInStorage) {
+//     if (localStorage.getItem(token) === null) {
+//         tokenInStorage = false;
+//     }
+//     else {
+//         token += 1;
+//     }
+// }
