b940b67c4e2adbab69ae7468a84894b0fec69e57
unknown <4500279@sps-student.sk.ca>
2019-01-10 14:16:38 -0600

    Saving Finally Done. Working on displaying

--- majorproject/sketch.js
+++ majorproject/sketch.js
@@ -2,19 +2,17 @@
 // Ethan Church
 // Start December 10, 2018
 
-let tasks;
-let mytask;
-let task;
+let task, itmes;
 let inputs;
 let itemNumber, itemInStorage;
 
 
 function setup(){
+  display();
   itemNumber = 0;
   itemInStorage = true;
-  formControl();
-  display();
   newItem();
+  formControl();
 }
 
 //Creating form and controls
@@ -26,7 +24,7 @@ function formControl(){
     userInput.attribute("placeholder", "Add new task");
     submitButton.parent("toDo");
     submitButton.class("btn btn-primary");
-    submitButton.keyPressed();
+    submitButton.mousePressed(store);
 }
 
 //Storing information
@@ -35,28 +33,21 @@ function store(){
   inputs = userInput.value();
   task = {
     name: inputs,
+    due: "Tommorow",
   }
+
   //Saving to local storage
-  localStorage.setItem(itemNumber(), task);
-  // if (localStorage.getItem(task.name) === null){
-  //   taskArray = [];
-  //   taskArray.push(task.name);
-  //   localStorage.setItem('task', (task.name));
-  // } else {
-  //   myTasks = (localStorage.getItem('taskArray'));
-  //   myTasks.push(taskArray);
-  //   localStorage.setItem('task', myTasks);
-  // }
-  display();
+  items = JSON.stringify(task);
+  localStorage.setItem(itemNumber, items);
+  localDisplay();
 }
 
+//Displays input from local storage
 function display(){
   for (let i = 0; i < localStorage.length; i++){
-    output = localStorage.getItem(1);
-    outputFinal = createElement("input", output);
-    outputFinal.parent("output");
-    outputFinal.class("form-check-input");
-    console.log(output);
+    output = JSON.parse(localStorage.getItem(1)).name;
+    outputStyle = createElement("input", output);
+    outputStyle.parent("output");
   }
 }
 
@@ -70,48 +61,3 @@ function newItem(){
       }
   }
 }
-
-function keyPressed(){
-  if (keyCode === Enter){
-    store();
-  } else if (keyCode === ){
-    store();
-  }
-}
-
-
-// let task = {name: "Buy Milk", due: "tomorrow", who: "me"}
-// undefined
-// task.due
-// "tomorrow"
-// task.name
-// "Buy Milk"
-// JSON.stringify(task)
-// "{"name":"Buy Milk","due":"tomorrow","who":"me"}"
-// let testingThing = JSON.stringify(task)
-// undefined
-// JSON.parse(testingThing).due
-// "tomorrow"
-// localStorage.setItem(6, testingThing);
-// undefined
-// localStorage.getItem(6)
-// "{"name":"Buy Milk","due":"tomorrow","who":"me"}"
-// JSON.parse(localStorage.getItem(6)).due
-// "tomorrow"
-// JSON.parse(localStorage.getItem(6)).name
-// "Buy Milk"
-// localStorage.has(0)
-
-
-
-// token = 0;
-// tokenInStorage = true;
-// true
-// while (tokenInStorage) {
-//     if (localStorage.getItem(token) === null) {
-//         tokenInStorage = false;
-//     }
-//     else {
-//         token += 1;
-//     }
-// }
